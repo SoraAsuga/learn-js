a73c9031c3be54517b0bf1a05112f480cc571d11
CryUshio <1151291182@qq.com>
2021-08-15 14:41:25 +0800

    feat: update

---
+++ 10、事件/2.操作 DOM/index.js
@@ -0,0 +1,13 @@
+/**
+ * 1.创建 style 元素，编写样式将 id 为 draft 的元素边框设置为黑色，插入到 head
+ * 2.循环插入 10 个 p 标签，内容分别为从 1 到 10 递增的自然数
+ * 3.获取第 8 个 p 标签，并将其文本改为蓝色
+ * 4.获取第 8 个 p 标签的前后所有兄弟节点
+ * 5.创建 style 元素，设置 highlight 类的文本颜色为红色，插入到 head
+ * 6.点击 button 按钮时，使用 classList 给文本为 draft 的元素添加 highlight 类，再次点击后取消添加
+ * 7.给 button 添加自定义属性 glj，值为 233，当点击 button 时打印自定义属性 glj 的值
+ * 8.获取 button 的 innerHTML 和 outerHTML 并理解
+ * 9.设置 button 的文本为 toggle draft
+ * 10.获取 button 的父节点，并获取 button 的宽高、相对页面的 x y 坐标（p321）
+ * 11.理解 offsetXXX、clientXXX、scrollXXX 三种位置属性的用法（p321）
+ */
