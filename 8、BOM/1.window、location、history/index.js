f61b33c62c712eed7b406ec916243ef66060f6a1
CryUshio <1151291182@qq.com>
2021-08-22 18:29:43 +0800

    feat: update

--- 8、BOM/1.window、location、history/index.js
+++ 8、BOM/1.window、location、history/index.js
@@ -10,43 +10,54 @@
  */
 
 console.log(window.screenTop, window.screenLeft, window.innerHeight, window.innerWidth, window.outerHeight, window.outerWidth);
-const x = (1040 - Number(window.outerHeight)) / 2;
-const y = (1920 - Number(window.outerWidth)) / 2
-window.moveTo(x, y);
-window.innerWidth = 1280;
-window.innerHeight = 720;
-const Baidu = window.open('https://www.baidu.com/', 'BaiDu');
-setTimeout(function () {
-  Baidu.close()
-}, 5000);
+/** 必须由此页面打开的窗口才能控制 */
+// const Baidu = window.open('http://127.0.0.1:5500/8%E3%80%81BOM', 'BaiDu', "resizable,scrollbars,status");
+// Baidu.resizeTo(1280, 720);
+// const x = (screen.width - Baidu.outerWidth) / 2;
+// const y = (screen.height - Baidu.outerHeight) / 2;
+// Baidu.moveTo(x, y);
+// setTimeout(() => {
+//   Baidu.close();
+// }, 5000);
 
 /**
  * 1.获取当前网页地址
  * 2.获取当前网页域名
- * 3.修改当前网页地址，在地址后面加上 '?glj=1&learn=2'
+ * 3.修改当前网页地址，在地址后面加上查询字符串 '?glj=1&learn=2'
  * 4.获取当前网页查询字符串，并转换为 { glj: 1, learn: 2 } 的形式
  * 5.获取当前网页哈希（hash）
  */
 
 console.log('地址', window.location.href, '域名', window.location.host);
-location.hash = '?glj=1&learn=2';
+if (!location.search) {
+  location.search = '?glj=1&learn=2';
+}
 
 function getQueryStringArgs() {
-  const qs = (location.search.length > 0 ? location.search.substring(1) : '');
+  /** 此处 location.search 如果不存在会返回空字符串，因此不用担心报错 */
+  const qs = location.search.substring(1);
   const args = {};
-  let items = qs.length ? qs.split('&') : [];
-  let item = null;
-  let name = null;
-  let value = null;
+  const items = qs.length ? qs.split('&') : [];
+  console.log('items:', items);
   let len = items.length;
-
+  
   for (let i = 0; i < len; i++) {
-    item = items[i].split('=');
-    name = decodeURIComponent(item[0]);
-    value = decodeURIComponent(item[1]);
+    const item = items[i].split('=');
+    const name = decodeURIComponent(item[0]);
+    const value = decodeURIComponent(item[1]);
 
-    args[i] = toString(name) + ': ' + toString(value);
+    args[name] = value;
   }
+
+  console.log('reduce', items.reduce((qsObject, current) => {
+    const item = current.split('=');
+    const name = decodeURIComponent(item[0]);
+    const value = decodeURIComponent(item[1]);
+    qsObject[name] = value;
+
+    return qsObject;
+  }, {}));
+
   return args;
 }
 console.log(getQueryStringArgs());
@@ -58,8 +69,8 @@ console.log(location.hash);
  * 3.跳转到百度
  */
 
-history.go(-1);
-/** history.back() */
-history.go(1);
-/** history.forward() */
-history.go('www.baidu.com');
+// history.go(-1);
+// /** history.back() */
+// history.go(1);
+// /** history.forward() */
+// history.go('www.baidu.com');
